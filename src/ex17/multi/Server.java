66c4ac9ec3b3f1bf52086d652cf0989e7b835bb4
khh <khh8500@naver.com>
2024-01-10 15:32:01 +0900

    전이중 공부

---
+++ src/ex17/multi/Server.java
@@ -0,0 +1,38 @@
+package ex17.multi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            // 1. 소켓과 버퍼 만들기
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
+
+
+            BufferedReader br = new BufferedReader(
+              new InputStreamReader(socket.getInputStream())
+            );
+
+            // 2. 메시지 받기 스레드
+            new Thread(() -> {
+            while (true) {
+                try {
+                    String requestMsg = br.readLine();
+                    System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }}).start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
